b77c745ac9030dd01a3d5e2e9ae1b267874876e0
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2023-11-19 12:47:47 +0600

    Change from Rafi Sharkar_0777

--- src/Components/Pages/QuizPage/Question.js
+++ src/Components/Pages/QuizPage/Question.js
@@ -5,6 +5,7 @@ export default function Question() {
   return (
     <div className='Qbody'>
         <h3>Qno: Who is not a Victorian poet?</h3>
+        <h6> Well done for your Exam</h6>
     </div>
   )
 }
