a4ac45b55dfaf4e896806907809094593404580d
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2023-11-20 09:16:09 +0600

    SunDay's problem is fixed

--- src/Components/Pages/QuizPage/Question.js
+++ src/Components/Pages/QuizPage/Question.js
@@ -5,6 +5,7 @@ export default function Question() {
   return (
     <div className='Qbody'>
         <h3>Qno: Who is not a Victorian poet?</h3>
+        <h4> Problem is fixed, </h4>
     </div>
   )
 }
