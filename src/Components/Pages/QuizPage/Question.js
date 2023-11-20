9bde3d5c9eb8c0e5f122bdd2221888151cbc09bd
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2023-11-20 11:07:26 +0600

    'commit_04'

--- src/Components/Pages/QuizPage/Question.js
+++ src/Components/Pages/QuizPage/Question.js
@@ -5,7 +5,6 @@ export default function Question() {
   return (
     <div className='Qbody'>
         <h3>Qno: Who is not a Victorian poet?</h3>
-        <h4> Problem is fixed, </h4>
     </div>
   )
 }
--- src/Components/Pages/QuizPage/Questions.js
+++ src/Components/Pages/QuizPage/Questions.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import Question from './Question'
 import './Questions.css'
-import { FaQuestionCircle } from "react-icons/fa";
 
 export default function Questions() {
   return (
     <div>
-      <div>
-        <h3>45th BCS (Preli)</h3>
+      <div className='Qtitle'>
+        <p>44th BCS (Preli)</p>
         <div className='Qname'>
-          <h6>General</h6>
-          <h6>19 May 2023</h6>
-          <h6>Marks 200</h6>
+          <p>General</p>
+          <p>19 May 2023</p>
+          <p>Marks 200</p>
         </div>
       </div>
+      <h3>কোড : 1 কোড ; হেমন্ত</h3>
       <Question />
-      <FaQuestionCircle />
-      <i class="fa-solid fa-question"></i>
     </div>
   )
 }
